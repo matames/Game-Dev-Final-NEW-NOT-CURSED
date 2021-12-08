94eff990ae7a4ebbb30e79f10004a79f31b959c2
Amiya <anl3488@nyu.edu>
2021-12-08 16:20:38 -0500

    Walking animations again
    - there is an issue with the movement like she moves REALLY slow even when i raise the speed
    - also the mini game came up HUGE so when i added the character I had to scale her up by a lot, maybe that's what's affecting the movement?

--- Assets/Amiya/Scripts/movement.cs
+++ Assets/Amiya/Scripts/movement.cs
@@ -6,10 +6,14 @@ public class movement : MonoBehaviour
 {
     public float speed;
 
+    public Animator animator;
+
+    SpriteRenderer myRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -20,19 +24,47 @@ public class movement : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             pos.x += speed * Time.deltaTime;
+
+            myRenderer.flipX = false;
+
+            animator.SetBool("side", true);
+            animator.SetBool("back", false);
+            animator.SetBool("walking", true);
         }
         else if (Input.GetKey(KeyCode.A))
         {
             pos.x -= speed * Time.deltaTime;
+
+            myRenderer.flipX = true;
+
+            animator.SetBool("side", true);
+            animator.SetBool("back", false);
+            animator.SetBool("walking", true);
+        }
+        else
+        {
+            animator.SetBool("walking", false);
         }
 
         if (Input.GetKey(KeyCode.W))
         {
             pos.y += speed * Time.deltaTime;
+
+            animator.SetBool("back", true);
+            animator.SetBool("side", false);
+            animator.SetBool("walking", true);
         }
         else if (Input.GetKey(KeyCode.S))
         {
             pos.y -= speed * Time.deltaTime;
+
+            animator.SetBool("back", false);
+            animator.SetBool("side", false);
+            animator.SetBool("walking", true);
+        }
+        else if(!animator.GetBool("walking"))       // if player is not walking to the side
+        {
+            animator.SetBool("walking", false);
         }
 
         transform.position = pos;
